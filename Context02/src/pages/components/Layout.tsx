dc59191f56b334ee7c19ca3be5f921628eb952db
ゆうご <yuhgo090764@gmail.com>
2022-08-06 16:03:11 +0900

    ✨ useContext02

---
+++ Context02/src/pages/components/Layout.tsx
@@ -0,0 +1,18 @@
+import { FC, ReactNode } from "react";
+import { Header } from "./Header";
+
+type Props = {
+  children: ReactNode;
+  todoCount: number;
+};
+
+export const Layout: FC<Props> = (props) => {
+  const { children, todoCount } = props;
+
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
